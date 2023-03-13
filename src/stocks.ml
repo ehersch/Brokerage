5831220e63e978be39a93b939167be924fc042a6
Jerry Chen <jwc279@cornell.edu>
2023-03-13 14:04:33 -0400

    fixed some dependency issues for Stocks.ml

--- src/stocks.ml
+++ src/stocks.ml
@@ -1,3 +1,17 @@
+open Cohttp_lwt_unix
 open Cohttp
 open Yojson
 open Lwt
+open Unit
+
+let url = Uri.of_string "https://api.example.com/data"
+
+let calltest =
+  "https://api.polygon.io/v2/aggs/ticker/AAPL/range/1/day/2023-01-09/2023-01-09?apiKey=jn_NAmtAD16hk6azpunzVK1TEvKiu5vy"
+
+let headers =
+  Header.init_with "Authorization" "Bearer jn_NAmtAD16hk6azpunzVK1TEvKiu5vy"
+
+let request =
+  Client.get ~headers url print_endline
+    "Put the ticker symbol in of the stock you would like to view."
