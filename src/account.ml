3dfc9b80e1608e1136dad5e610360fc841a8a851
Ethan <esh87@cornell.edu>
2023-03-17 17:06:49 -0400

    3/19 Work session:
    - Began implementing account
    - Working with try-catch in stocks

--- src/account.ml
+++ src/account.ml
@@ -1,9 +1,59 @@
+exception Broke
+(** Raised when attempting to withdraw more money than available.*)
+
+exception OverMemory
+(** Raised when *)
+
 type stock = {
   ticker : string;
   price : float;
 }
+(** Type [stock] contains the ticker symbol [ticker] and current price [price]. *)
 
 type account = {
-  balance : float;
-  portfolio : stock list;
+  stock_balance : float;
+  cash_balance : float;
+  portfolio : stock * float * float list;
 }
+(** Type [account] contains the floats [stock_balance] and [cash_balance] and
+    the set-like-list of stocks [portfolio] and how many shares owned. *)
+
+type transaction = {
+  time : float;
+  type_of_transaction : string;
+  change : float;
+}
+
+(* Returns: [account amt acc] a record with updated [stock_balance] and
+   [cash_balance] after withdrawing the specified amount and requires: - [amt]
+   is a non-negative float representing the amount to be withdrawn. - [acc]: A
+   valid account record with at least the same amount of cash_balance as the
+   requested withdrawal amount. - Raises: [Broke] when the requested withdrawal
+   amount is greater than the available cash_balance in the account. - Example:
+   given an account with [cash_balance] = 1000.0, [stock_balance] = 500.0, and
+   portfolio, withdrawing 200.0 will result in an account with cash_balance =
+   800.0 and stock_balance = 300.0. *)
+let withdraw (amt : float) (acc : account) =
+  if amt > acc.cash_balance then raise Broke
+  else
+    {
+      stock_balance = acc.stock_balance -. amt;
+      cash_balance = acc.cash_balance -. amt;
+      portfolio = acc.portfolio;
+    }
+
+(* Returns: [deposit amt acc] a new account record with an updated cash_balance
+   after depositing the specified amount and requires: - [amt]: A non-negative
+   float representing the amount to be deposited. - [acc]: A valid account
+   record. - The total account stock_balac cannot be over 1073741823 - Raises:
+   None - Examples: Given an account with cash_balance = 1000.0, depositing
+   200.0 will result in an account with cash_balance = 1200.0. If the deposit
+   amount is 0.0, the function will return an account with the same
+   cash_balance. *)
+
+let deposit (amt : float) (acc : account) =
+  {
+    acc with
+    cash_balance = acc.cash_balance +. amt;
+    stock_balance = acc.stock_balance +. amt;
+  }
--- src/stocks.ml
+++ src/stocks.ml
@@ -8,26 +8,29 @@ open Unix
 
 let base_url = "https://api.polygon.io"
 
-(*Personal key for our team. Max calls per min = 5*)
+(** Personal key for our team. Max calls per min = 5*)
 let api_key = "jn_NAmtAD16hk6azpunzVK1TEvKiu5vy"
 
-(**Gets the uppercase ticker symbol of the stock the user wants info on.*)
+(** Gets the uppercase ticker symbol of the stock the user wants info on.*)
 let ticker =
   String.uppercase_ascii
     (print_endline "Enter the ticker symbol of a stock: \n";
      print_string "> ";
      read_line ())
 
-(**Formats the [year] properly because Unix has some interesting formatting for
-   years.*)
+(** - Returns: [proper_year year] the stringified year of the time field [year].
+    - Example: [proper_year 123] is "2023". *)
 let proper_year year = string_of_int (year + 1900)
 
-(**If the date field [date] has only one digit, this method places a zero before
-   that digit.*)
+(** - Returns: [proper_date date] the stringified integer which always has two
+      digits [date].
+    - Requires: [date] is an integer from 0 to 31.
+    - Example: [proper_date 8] is "08". *)
 let proper_date date =
   if date < 10 then "0" ^ string_of_int date else string_of_int date
 
-(**Returns the current date in year-month-date format*)
+(** - Returns: [get_date] the current date in year-month-day format
+    - Example: [get_date] today is 2023-03-19. *)
 let get_date =
   let t = Unix.localtime (Unix.time ()) in
   proper_year t.tm_year ^ "-"
@@ -35,21 +38,27 @@ let get_date =
   ^ "-"
   ^ proper_date (t.tm_mday - 1)
 
-(*[get_ticker_price] grabs symbol [ticker] and retrieves the closing price from
-  yesterday*)
+exception NoSuchStock of string
+(** Raised when this ticker symbol does not exist. *)
+
+(** - Returns: [get_ticker_price ticker] grabs the symbol [ticker] and retrieves
+      the closing price from yesterday.
+    - Raise: exception [NoSuchStock] if ticker symbol does not exist. *)
 let get_ticker_price ticker =
-  let endpoint =
-    "/v2/aggs/ticker/" ^ ticker ^ "/range/1/day/" ^ get_date ^ "/" ^ get_date
-    ^ "?adjusted=true&sort=asc&limit=120"
-  and query_params = [ ("apiKey", api_key) ] in
-  let uri =
-    Uri.with_query' (Uri.of_string (base_url ^ endpoint)) query_params
-  in
-  let%lwt response, body = Client.get uri in
-  let%lwt body_str = Cohttp_lwt.Body.to_string body in
-  let body_json = Yojson.Basic.from_string body_str in
-  let view_info =
-    body_json |> member "results" |> to_list |> List.hd |> member "c"
-    |> to_float
-  in
-  Lwt.return view_info
+  try
+    let endpoint =
+      "/v2/aggs/ticker/" ^ ticker ^ "/range/1/day/" ^ get_date ^ "/" ^ get_date
+      ^ "?adjusted=true&sort=asc&limit=120"
+    and query_params = [ ("apiKey", api_key) ] in
+    let uri =
+      Uri.with_query' (Uri.of_string (base_url ^ endpoint)) query_params
+    in
+    let%lwt response, body = Client.get uri in
+    let%lwt body_str = Cohttp_lwt.Body.to_string body in
+    let body_json = Yojson.Basic.from_string body_str in
+    let view_info =
+      body_json |> member "results" |> to_list |> List.hd |> member "c"
+      |> to_float
+    in
+    Lwt.return view_info
+  with Yojson.Basic.Util.Type_error (msg, _) -> raise (NoSuchStock ticker)
