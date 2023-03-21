b61bc50c96d68aa1ab092325ad49396381a6f237
Mohammad Islam <mai54@cornell.edu>
2023-03-21 18:37:56 -0400

    Implemented parsing in command.ml

--- src/command.ml
+++ src/command.ml
@@ -1,14 +1,14 @@
-type command = 
-| Bal of string list
-| Portfolio of string list
-| Dep of string list
-| Withdraw of string list
-| Help of string list
-| Quit 
+type command =
+  | Bal of string list
+  | Portfolio of string list
+  | Dep of string list
+  | Withdraw of string list
+  | View of string list
+  | Help
+  | Quit
 
 exception Empty
 exception Invalid
-
 
 (** only_spaces [list] takes a string list as input and returns a bool of
     whether or not the list contains only empty strings*)
@@ -17,7 +17,7 @@ let rec only_spaces list =
   | [] -> true
   | h :: t -> h = "" && only_spaces t
 
-  (** real_words [list] [acc] returns a list of strings that are not empty strings
+(** real_words [list] [acc] returns a list of strings that are not empty strings
     in list. If there are no non-empty strings in the list, returns the empty
     list*)
 let rec real_words list acc =
@@ -29,4 +29,27 @@ let rec real_words list acc =
         real_words t new_acc
       else real_words t acc
 
-let parse str  = let words = String.split_on_char ' ' str in 
+let rec get_first list =
+  if only_spaces list then list
+  else
+    match list with
+    | [] -> []
+    | h :: t ->
+        if
+          h = "-bal" || h = "-portfolio" || h = "-dep" || h = "-withdraw"
+          || h = "-view" || h = "-help" || h = "-quit"
+        then list
+        else if h = "" then get_first t
+        else [ "quit"; "invalid" ]
+
+let parse str =
+  let words = String.split_on_char ' ' str in
+  let actual_words = get_first words in
+  match actual_words with
+  | [ "-quit" ] -> Quit
+  | "-quit" :: t -> if only_spaces t then Quit else raise Invalid
+  | [ "-help" ] -> Help
+  | "-help" :: t -> if only_spaces t then Help else raise Invalid
+  | _ -> raise Invalid
+  | _ :: t -> if only_spaces words then raise Empty else raise Invalid
+  | [] -> raise Empty
