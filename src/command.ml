2eb6970bdc21fe3ce2b1f735edf50b7dd5a7ae43
Mohammad Islam <mai54@cornell.edu>
2023-03-21 20:54:07 -0400

    Implemented command.ml to parse fully
    - Added test cases in test/main.ml

--- src/command.ml
+++ src/command.ml
@@ -1,6 +1,6 @@
 type command =
-  | Bal of string list
-  | Portfolio of string list
+  | Bal
+  | Portfolio
   | Dep of string list
   | Withdraw of string list
   | View of string list
@@ -10,14 +10,14 @@ type command =
 exception Empty
 exception Invalid
 
-(** only_spaces [list] takes a string list as input and returns a bool of
+(** [only_spaces list] takes a string list as input and returns a bool of
     whether or not the list contains only empty strings*)
 let rec only_spaces list =
   match list with
   | [] -> true
   | h :: t -> h = "" && only_spaces t
 
-(** real_words [list] [acc] returns a list of strings that are not empty strings
+(** [real_words list acc] returns a list of strings that are not empty strings
     in list. If there are no non-empty strings in the list, returns the empty
     list*)
 let rec real_words list acc =
@@ -29,6 +29,10 @@ let rec real_words list acc =
         real_words t new_acc
       else real_words t acc
 
+(** [get_first list] returns the the tail of the list where the head is "go" or
+    "quit". If the list is only empty strings then returns the original list. If
+    there is no instance of "go" or "quit found in the list, returns a string
+    list that will raise malformed in parse str"*)
 let rec get_first list =
   if only_spaces list then list
   else
@@ -42,14 +46,47 @@ let rec get_first list =
         else if h = "" then get_first t
         else [ "quit"; "invalid" ]
 
+(** [command_help str output] returns what command type the given string is if
+    it requires extra parameters given the first word of the user input as str
+    and the rest of the words as output *)
+let command_help str output =
+  match str with
+  | "-bal" -> Bal
+  | "-portfolio" -> Portfolio
+  | "-dep" -> Dep output
+  | "-withdraw" -> Withdraw output
+  | "-view" -> View output
+  | _ -> raise Invalid
+
+(** [comp_mem str] checks if the given first word of a user input is a valid
+    command type listed in -help that needs extra parameters. Examples: -dep
+    500. or -withdraw 200.*)
+let comp_mem str =
+  str = "-bal" || str = "-dep" || str = "-withdraw" || str = "-portfolio"
+  || str = "-view"
+
+(** [com_mem str] checks whether the given first word of a user input is a
+    command type listed in -help that does not need extra parameters. Examples:
+    \-bal or -portfolio *)
+let com_mem str =
+  str = "-bal" || str = "-portfolio" || str = "-quit" || str = "-help"
+
+(** [parse str] parses a user's input into a [command]. The first word that is
+    not an empty string becomes the command type listed in -help and the rest of
+    the words if any become the phrase. Example:
+
+    - parse " -dep 500." is [Dep \["500."\]]
+    - parse "-bal" is [Bal] *)
 let parse str =
   let words = String.split_on_char ' ' str in
   let actual_words = get_first words in
   match actual_words with
-  | [ "-quit" ] -> Quit
-  | "-quit" :: t -> if only_spaces t then Quit else raise Invalid
-  | [ "-help" ] -> Help
-  | "-help" :: t -> if only_spaces t then Help else raise Invalid
+  | h :: t ->
+      if only_spaces actual_words then raise Empty
+      else if com_mem h then
+        if only_spaces t then command_help h [] else raise Invalid
+      else if comp_mem h then
+        let rest = real_words t [] in
+        if rest = [] then raise Empty else command_help h rest
+      else raise Invalid
   | _ -> raise Invalid
-  | _ :: t -> if only_spaces words then raise Empty else raise Invalid
-  | [] -> raise Empty
--- test/main.ml
+++ test/main.ml
@@ -1,2 +1,32 @@
 open Stocks
 open OUnit2
+
+(* Parse tests for valid user input commands*)
+let parse_test (name : string) (input_str : string) (expected_output : command)
+    : test =
+  name >:: fun _ -> assert_equal expected_output (parse input_str)
+
+(* Parse tests for user inputs that will raise exception Empty*)
+let parse_excE_test (name : string) (input_str : string) : test =
+  name >:: fun _ -> assert_raises Empty (fun () -> parse input_str)
+
+(* Parse tests for user inputs that will raise Invalid for invalid commmands*)
+let parse_excI_test (name : string) (input_str : string) : test =
+  name >:: fun _ -> assert_raises Invalid (fun () -> input_str)
+
+let command_tests =
+  [
+    parse_test "Testing a valid single parameter command" "  -bal  " Bal;
+    parse_test "Testing a valid command that uses multiple parameters"
+      "-dep 500.0" (Dep [ "500.0" ]);
+    parse_test "Testing a valid command with multiple spaces in between"
+      " -withdraw     300.0 " (Withdraw [ "300.0" ]);
+    parse_excE_test "Testing an empty string" "";
+    parse_excE_test "Testing a string with only spaces" "      ";
+    parse_excI_test "Testing a string with words after a no parameter command"
+      "-bal invalid";
+    parse_excI_test
+      "Testing a string with no words after a parameter involved command" "-dep";
+    parse_excI_test "Testing a string without a first word as a valid command"
+      "let's -dep 500";
+  ]
