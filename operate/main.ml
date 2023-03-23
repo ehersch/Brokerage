e22ca0f1f41ba028af21b11d49bd6fee2f1d4cfb
Jerry Chen <jwc279@cornell.edu>
2023-03-22 20:36:50 -0400

    Fixed make view_stock bugs

--- operate/main.ml
+++ operate/main.ml
@@ -4,9 +4,13 @@ open Stocks
 let rec view_ticker () =
   try
     let ticker_price = Lwt_main.run (get_ticker_price ticker) in
+    print_endline "\nRemember, the API only has 5 calls per minute.\n";
     print_endline
       ("\nThe closing stock price for "
       ^ String.uppercase_ascii ticker
       ^ " yesterday was\n"
       ^ string_of_float ticker_price)
-  with _ -> view_ticker ()
+  with _ ->
+    print_endline
+      "Invalid Stock Ticker. Run make view_stock to begin again. \n\
+      \      Remember, the API only has 5 calls per minute."
